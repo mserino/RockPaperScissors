779a399c4b51781f96bd196d6f78f82f2df3799e
Margherita <m.serino@hotmail.it>
2014-06-23 14:23:04 +0100

    Test passing

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,3 +1,11 @@
+var rock, paper, scissors, beats;
+
+var pairs = {
+	"rock": {"beats": "scissors"},
+	"paper": {"beats": "rock"},
+	"scissors": {"beats": "paper"}
+};
+
 function Player(name) {
   this.name = name;
 };
@@ -12,7 +20,12 @@ function Game(player1, player2) {
 };
 
 Game.prototype.winner = function() {
-  if(1 === 1) {
+	if (this.player1.pick === this.player2.pick) {
+		return null
+	} 
+	else if (pairs[this.player1.pick].beats === this.player2.pick) {
     return this.player1;
+  } else {
+  	return this.player2;
   }
-};
+}
